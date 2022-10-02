9d57ae81c1fd3b3dc3a7432925ef3022e688e4d0
Ashraful Islam <ashrafulrobin3@gmail.com>
2022-10-02 18:50:25 +0600

    Initial commit. Project bootstrapping...

---
+++ README.md
@@ -0,0 +1,2 @@
+## Playing with PostgreSQL
+> This repository is for using the postgres resource and how we can utilize nicely.
---
+++ scripts/1-init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE play_with_postgres;
+CREATE USER postgres WITH PASSWORD postgres;
+GRANT ALL PRIVILEGES ON DATABASE play_with_postgres to postgres;
